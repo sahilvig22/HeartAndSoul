9492f6532f61a3b2be695a134cece18be44be015
sahilvig22 <thesahilvig@gmail.com>
2020-03-22 19:00:57 +0530

    index

---
+++ index.php
@@ -0,0 +1,156 @@
+<html>
+	<head>
+		<title>Heart&Soul</title>
+		<link rel="stylesheet" href="css/style.css"/>
+
+
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td> <?php include'header.php'; ?> </td>
+			</tr>
+		</table>
+
+		
+
+		
+		<div id="outerbox">
+			<div id="sliderbox" >
+				<img src="images/slider6.jpg">
+				<img src="images/slider5.jpg">
+				<img src="images/slider1.gif">
+				<img src="images/slider2.jpg">
+				<img src="images/slider3.png">
+				<img src="images/slider4.jpg">
+			</div>
+		</div>
+
+
+		<div class="wrapper">
+
+			<div class="customers">
+				<div class="showcase">
+
+
+						<h1>Happy Customers</h1>
+
+						<ul>
+							<li><h2>Rahul, New Delhi : </h2>Best cards ever!</li>
+							<li><h2>Shweta, Mumbai : </h2>Gonna Order More!</li>
+							<li><h2>Siddharth, Lucknow : </h2>Good Quality cards!</li>
+							<li><h2>Lakshay, Hyderabad : </h2>I Ordered 5 :)</li>
+							<li><h2>Rupali, Bangalore : </h2>The Best In Town :)</li>
+							<li><h2>Bhanu, New Delhi : </h2>Fast Delivery!</li>
+							<li><h2>Sanjay, Uttranchal : </h2>Just Loved them <3</li>
+							<li><h2>Ron, Uttar Pradesh : </h2>Affordable and Nice :)</li>
+							<li><h2>Astha, Gurugran : </h2>Superior Quality Cards :)</li>
+							<li><h2>Bhavleen, Kerela : </h2>Will Always Remember this!</li>
+							<li><h2>Akash, New Delhi : </h2>So Many To Choose From :)</li>
+							<li><h2>Seema,New Delhi : </h2>Amazing Quality :)</li>
+
+
+						</ul>
+
+						<div class=sub-grid2 >
+							<h1>Happy Customers</h1>
+							<ul>
+								<marquee direction="up" width="200px" height="500px" onmouseover="this.stop()" onmouseout="this.start()" behavior="scroll">
+									<li><h2>Rahul, New Delhi : </h2>Best cards ever!</li><br>
+									<li><h2>Shweta, Mumbai : </h2>Gonna Order More!</li><br>
+									<li><h2>Sid, Lucknow : </h2>Good Quality cards!</li><br>
+									<li><h2>Lucky, Hyderabad : </h2>I Ordered 5 :)</li><br>
+									 <li><h2>Rupali, Bangalore : </h2>The Best In Town :)</li><br>
+									<li><h2>Bhanu, New Delhi : </h2>Fast Delivery!</li><br>
+									<li><h2>Sunny, New Delhi : </h2>Just Loved them <3</li><br>
+									<li><h2>Ron, UP : </h2>Affordable and Nice :)</li><br>
+									<li><h2>Astha, Gurugran : </h2>Superior Quality Cards :)</li><br>
+									<li><h2>Bhavleen, Kerela : </h2>Will Always Remember this!</li><br>
+									<li><h2>Akash, New Delhi : </h2>So Many To Choose From :)</li><br>
+									<li><h2>Seema,New Delhi : </h2>Amazing Quality :)</li><br>
+									</marquee>
+									</ul>
+								
+						</div>
+
+				   
+				</div>
+			</div>
+
+			<div class="upcoming_products">
+				<div class="showcase">
+					<h1 style="margin:5px 5px; padding: 5px 5px; letter-spacing: 0.2em;">Upcoming Products at <strong>Heart & Soul.com</strong></h1>
+					<p style="margin:5px 5px; padding:0px 5px 15px 5px;font-size: 20px;">At <strong>Heart & Soul</strong> quality and selection is never compromised. We understand the emotions our cards deliver and hence keep updating ourselves. We keep coming up with new colors and designs of cards. Some of our upcoming cards are:</p>
+
+					<div class="sub-grid3"  style="background-image:url(images/abstract.jpg); background-size:cover;">
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;"src="images/upcoming1.jpg">
+						<p style="margin-top:50px;font-size: 20px;"> A Slide Open card. Will Be Introduced In All The Categories Available. Stay tuned...</p>
+
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;" src="images/upcoming2.jpg">
+						<p style="margin-top:50px;font-size: 20px;">Card In A Transparent Glass jar. Will Be Introduced In All The Categories Available.Stay tuned...</p>
+
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;" src="images/upcoming3.jpg">
+						<p style="margin-top:50px;font-size: 20px;">Card In A Woolen Fabric. Different Colors Will Be Available.Stay tuned...</p>
+
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;" src="images/upcoming4.jpg">
+						<p style="margin-top:50px;font-size: 20px;">Merry Christmas Card With Creative Opening. Stay tuned...</p>
+
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;" src="images/upcoming5.jpg">
+						<p style="margin-top:50px;font-size: 20px;">Happy Birthday Card With Creative Opening. Cream Color. Stay tuned...</p>
+
+						<img style=" border-radius:1.5em; width:200px; height:200px;margin-top:25px;" src="images/upcoming6.jpg">
+						<p style="margin-top:50px;font-size: 20px;">Happy Birthday Card With Creative Opening. Red Color. Stay tuned...</p>
+
+					</div>
+				</div>
+
+			</div>
+
+
+		</div><!--end of wrapperclass-->
+
+		 <ul id="wire">
+
+		<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+	</ul>
+
+  
+	   
+	
+		
+		 <div class="footer"> Made By Sahil Vig &copy; 2018 </div> 
+
+
+	</body>
+</html>
