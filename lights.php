eb4934185abb3ed3d0aeee23be920ee7ae019b38
sahilvig22 <thesahilvig@gmail.com>
2020-03-22 19:00:33 +0530

    lights

---
+++ lights.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+
+        <ul id="wire">
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>      
+        </ul>
+       
+
+            
+
+
+
+            </div>
+        </div>
+    </body>
+</html>
