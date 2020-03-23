1e070431ff78a1b27ca1511f61ee80e71b7e507c
sahilvig22 <thesahilvig@gmail.com>
2020-03-23 21:18:40 +0530

    header

---
+++ header.php
@@ -0,0 +1,84 @@
+
+<html>
+    <head>
+
+        <title>Major Project</title>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico" >
+        
+    </head>
+    <body style="font-family: 'Pacifico', cursive;">
+
+        
+
+            <div class="heading1">
+                <a href="index.php">Heart&Soul</a>
+
+            </div>
+
+            <div class="heading2">
+                <a href="index.php">It's the effort-not cards...</a>
+                <br> <br><br><br>
+            </div>
+    
+<script type="text/javascript">
+    var currentTime = new Date();
+    var m = new Array();
+    m[0] = "January";
+    m[1] = "February";
+    m[2] = "March";
+    m[3] = "April";
+    m[4] = "May";
+    m[5] = "June";
+    m[6] = "July";
+    m[7] = "August";
+    m[8] = "September";
+    m[9] = "October";
+    m[10] = "November";
+    m[11] = "December";
+    var month = m[currentTime.getMonth()];
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+    var hour = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    document.write("&nbsp; &nbsp; &nbsp;" + day + " " + month + " " + year);
+    document.write("&nbsp; &nbsp; &nbsp;" + hour + ":" + minutes + ":" + seconds);
+    
+
+</script>
+
+       
+            
+                 
+          
+    <nav id="navbar" style="position: relative;">
+
+        <ul>
+            <li><a href="index.php" class="nav-neon , navbarhover">Home</a></li>
+            
+            
+            <li><a href="about.php" class="nav-neon , navbarhover">About</a></li>
+            <li><a href="login.php" class="nav-neon , navbarhover">Log-in</a></li>
+        </ul>
+
+    
+    <marquee direction="left" scrollamount=20">
+        <img src="images/ribbon.jpg" style="width:100%;height:20px;margin-top: 20px; z-index: 0;">
+    </marquee>
+
+</nav>
+
+
+<div class="sociallinks">
+    <ul>
+        <li><a href="https://www.facebook.com/"><img src="Images/fb-logo.png"</a></li>
+        <li><a href="https://twitter.com/"><img src="Images/twitter-logo.png"</a></li>
+        <li><a href="https://www.snapchat.com/"><img src="Images/snap-logo.png"</a></li>
+        <li><a href="https://www.linkedin.com"><img src="Images/linkedin-logo.jpg"</a></li>
+        <li><a href="https://plus.google.com/discover"><img src="Images/google-logo.png"</a></li>
+    </ul>
+
+</div>
+</body>
+</html>
