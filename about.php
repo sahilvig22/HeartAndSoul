84c00ddf966a84b4731a8fc4d27b62d7b6c4b321
sahilvig22 <thesahilvig@gmail.com>
2020-03-25 02:01:30 +0530

    about

---
+++ about.php
@@ -0,0 +1,123 @@
+
+
+
+
+<head>
+    <title>About- Heart & Soul</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <table>
+        <tr>
+            <td>
+                <?php include "header.php"; ?>
+            </td>
+        </tr>
+    </table>
+
+
+
+
+
+    <p class= "mom" size="40">About</p>
+
+    <div class="hanging">
+        <div class="hang1">
+
+
+            <img src="Images/hang1.gif" >
+            <img src="Images/hang2.gif" >
+
+        </div>
+    </div>
+
+
+    <div class="upcoming_products">
+        <div class="showcase">
+            <h1 style="margin:5px 5px; padding: 5px 5px; letter-spacing: 0.2em;"></h1>
+            <p style="margin:5px 5px; padding:0px 5px 15px 5px;font-size: 20px;"></p>
+
+            <div class=sub-grid4 style="background-image:url(images/abstract.jpg); background-size:cover;" >
+                <h1 style="margin-left: 15px;letter-spacing: 0.1em;">About our company</h1>
+                <p style="letter-spacing: 0.1em; font-size: 20px; margin-top: 20px;margin-bottom: 40px;margin-right: 15px;margin-left: 15px;"><strong>Heart & Soul</strong> was established by Sahil Vig in 1996 with an aim to provide easy access 
+                    to people regarding gift cards and promoting how people did not need to visit a store, ask the 
+                    retailer to show them each card and get themselves in an embarrising situation when they are not 
+                    able to choose any of them. This is where <strong>Heart & Soul.com </strong>comes handy. You can 
+                    choose from a wide range of gift cards. We deal in a variety of cards and also keep updating our 
+                    website with the newest products. It is India's largest aggregator for gift cards, aims at 
+                    providing a hassle free and enjoyable shopping experience to shoppers across the globe with 
+                    the widest range of brands on its portal.</p>
+                
+                <h1 style="margin-left: 15px;letter-spacing: 0.1em;">Brands</h1>
+                <p style="letter-spacing: 0.1em; font-size: 20px; margin-top: 20px;margin-bottom: 40px;margin-right: 15px;margin-left: 15px;">GiftCardsIndia understands its shoppers' needs and caters to them with choice of gift 
+                    cards from over 200 leading brands. Prominent brands include Shoppers Stop, Lifestyle,
+                    Reliance, Big Bazaar, Titan, KFC, Book My Show, Amazon.in, Dominos Pizza, etc.</p>
+                <h1 style="margin-left: 15px;letter-spacing: 0.1em;">Recognition</h1>
+                <p style="letter-spacing: 0.1em; font-size: 20px; margin-top: 20px;margin-bottom: 40px;margin-right: 15px;margin-left: 15px;">Award of Excellence, 2011 at the e-Sparks Indian Ecommerce Showcase - Bangalore</p>
+                
+                <h1 style="margin-left: 15px;letter-spacing: 0.1em;">Do not know what to gift your loved ones?</h1>
+                <p style="letter-spacing: 0.1em; font-size: 20px; margin-top: 20px;margin-bottom: 40px;margin-right: 15px;margin-left: 15px;">Here is why gift cards are an ideal pick. Simply buying platitude gifts for your loved ones
+                    on occasions is just not happening. There are a lot of people who receive gifts from 
+                    somewhere and hand it over to someone else at the same time. Don not pick a gift that 
+                    can be easily passed on to someone else. Buying a gift voucher is a smarter idea as 
+                    it requires thought and does not look routine like all other gifts. However, if you
+                    are planning to buy a gift voucher of a particular store, you are restricting the 
+                    choice of the recipient. The gift voucher is valid for a one time use where the recipient 
+                    has to use the entire amount whereas a gift card allows you to avail benefits multiple 
+                    times.</p>
+
+
+                    <h1 style="margin-left: 15px;letter-spacing: 0.1em;">Talk to us</h1>
+                <p style="letter-spacing: 0.1em; font-size: 20px; margin-top: 20px;margin-bottom: 40px;margin-right: 15px;margin-left: 15px;">You can reach us on our 24 X 7 helpline number +9999667144 or mail us at customercare@heartandsoul.com. Happy Purchasing!</p>
+            </div>
+            
+        </div>
+
+    </div> 
+
+
+
+    <ul id="wire">
+
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+
+    </ul>
+
+</div>
+</div>
+
+<div class="footer"> Made By Sahil Vig &copy; 2018 </div> 
+
+</body>
